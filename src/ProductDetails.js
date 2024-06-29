8c0b3fb162891336e8444b7afece9f4a4049b8c8
kaushikkaju <68936917+kaushikkaju@users.noreply.github.com>
2024-06-30 00:12:07 +0530

    Product Details Update

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const ProductDetails = ({ }) => {
+const ProductDetails = ({ product, addtoCart, onClose }) => {
     return (
         <>
-            Hello
+            <button className='font-semibold bg-gray w-fit text-white px-3 py-1.5' onClick={onClose} >Close</button>
+            <div key={product.id} className="grid border-2 border-slate-100	p-5">
+                {/* Render product details here (e.g., title, description, price) */}
+                <img src={product.thumbnail} alt={product.image} className="h-100 justify-self-center" />
+                <h2 className="font-bold">{product.title}</h2>
+                <p>{product.description}</p>
+                <p className='font-bold text-uppercase'>{product.category}</p>
+                <button type="button" className="font-semibold bg-gray w-fit text-white px-3 py-1.5" onClick={() => addtoCart(product)}>Add to Cart</button>
+            </div>
         </>
     )
 }
